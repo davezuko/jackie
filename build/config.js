8b01d1d031cf928460bfe2eebf6b1666684fbc26
David Zukowski <zukoweb@gmail.com>
2015-02-25 15:16:35 -0500

    Moves client dist folder again (now builds to project_root/dist)

--- build/config.js
+++ build/config.js
@@ -27,7 +27,7 @@ exports.env.PROD = exports.env.PRODUCTION = 'production';
 exports.client        = {};
 exports.client.base   = path(exports.BASE, 'client');
 exports.client.src    = exports.client.base;
-exports.client.dest   = path(exports.client.base, 'dist');
+exports.client.dest   = path(exports.BASE, 'dist');
 exports.client.reload = undefined; // livereload port
 
 // --------------------------
