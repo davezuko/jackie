52a10e15e16fc01412b4f138477e98dd2afdfbc5
David Zukowski <zukoweb@gmail.com>
2015-03-06 16:52:26 -0500

    started adding styles to career page.

--- server/routes/views.js
+++ server/routes/views.js
@@ -33,12 +33,63 @@ module.exports = function (app, config) {
     });
   });
 
-  NAV_ITEMS.forEach(function (item) {
-    app.get(`${item.route}`, function (req, res) {
-      res.render(`${item.route.slice(1)}/index`, {
-        title  : item.name,
-        active : item.name
-      });
+  app.get('/about', function (req, res) {
+    res.render('about/index', {
+      title : 'About'
+    });
+  });
+
+  app.get('/bsw-education', function (req, res) {
+    res.render('bsw-education/index', {
+      title : 'BSW Education'
+    });
+  });
+
+  app.get('/gallery', function (req, res) {
+    res.render('gallery/index', {
+      title : 'Gallery'
+    });
+  });
+
+
+  app.get('/contact', function (req, res) {
+    res.render('contact/index', {
+      title : 'Contact'
+    });
+  });
+
+  // ----------------------------------
+  // Career Page
+  // ----------------------------------
+  app.get('/career', function (req, res) {
+    res.locals.careerItems = [
+      {
+        date   : 'Nov. 25, 2014',
+        labels : [
+          { heading : 'Test Label' }
+        ],
+        heading : 'Became official with this amazing guy named David.',
+        copy : [
+          `This is a bunch of test copy. Each item in the collection will be displayed as a paragraph! I\'m really not sure what else to put in here but really don't want to use more lorem ipsum. I could use Bacon Ipsum but that still isn't too exciting... it's much easier to just sit here and ramble in retrospect of everything.`,
+          `Notice how this is a different paragraph! How amazing. This is even more test copy because I need to see how this looks with actual copy. Just writing more and more, hopefully this ends soon.`
+        ]
+      },
+      {
+        date   : 'Jan. 28, 2015',
+        labels : [
+          { heading : 'Test Label' },
+          { heading : 'Test Label' }
+        ],
+        heading : 'Had an incredible birthday!',
+        copy : [
+          `This is a bunch of test copy. Each item in the collection will be displayed as a paragraph! I\'m really not sure what else to put in here but really don't want to use more lorem ipsum. I could use Bacon Ipsum but that still isn't too exciting... it's much easier to just sit here and ramble in retrospect of everything.`,
+          `Notice how this is a different paragraph! How amazing. This is even more test copy because I need to see how this looks with actual copy. Just writing more and more, hopefully this ends soon.`
+        ]
+      }
+    ];
+
+    res.render('career/index', {
+      title : 'Career'
     });
   });
 };
