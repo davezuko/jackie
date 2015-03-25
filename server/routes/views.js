23f5feb2186fa80810f25285cc5f49ed6ebc38a1
David Zukowski <zukoweb@gmail.com>
2015-03-24 22:30:21 -0400

    Heading improvements.

--- server/routes/views.js
+++ server/routes/views.js
@@ -4,6 +4,7 @@
 const NAV_ITEMS = [
   'BSW',
   'Resume',
+  'Goals',
   'Reflection',
   'Gallery',
   'Contact'
@@ -44,6 +45,18 @@ module.exports = function (app, config) {
     });
   });
 
+  app.get('/resume', function (req, res) {
+    res.render('resume/index', {
+      title : 'Resume'
+    });
+  });
+
+  app.get('/goals', function (req, res) {
+    res.render('goals/index', {
+      title : 'Goals'
+    });
+  });
+
   app.get('/reflection', function (req, res) {
     res.render('reflection/index', {
       title : 'Reflection'
@@ -57,50 +70,4 @@ module.exports = function (app, config) {
   });
 
   require('./views/contact')(app, config);
-
-  // ----------------------------------
-  // Resume Page
-  // ----------------------------------
-  app.get('/resume', function (req, res) {
-    res.locals.resumeItems = [
-      {
-        date   : 'Nov. 25, 2014',
-        labels : [
-          { heading : 'Test Label' }
-        ],
-        heading : 'Became official with this amazing guy named David.',
-        copy : [
-          `This is a bunch of test copy. Each item in the collection will be displayed as a paragraph! I\'m really not sure what else to put in here but really don't want to use more lorem ipsum. I could use Bacon Ipsum but that still isn't too exciting... it's much easier to just sit here and ramble in retrospect of everything.`,
-          `Notice how this is a different paragraph! How amazing. This is even more test copy because I need to see how this looks with actual copy. Just writing more and more, hopefully this ends soon.`
-        ]
-      },
-      {
-        date   : 'Jan. 28, 2015',
-        labels : [
-          { heading : 'Test Label' },
-          { heading : 'Test Label' }
-        ],
-        heading : 'Had an incredible birthday!',
-        copy : [
-          `This is a bunch of test copy. Each item in the collection will be displayed as a paragraph! I\'m really not sure what else to put in here but really don't want to use more lorem ipsum. I could use Bacon Ipsum but that still isn't too exciting... it's much easier to just sit here and ramble in retrospect of everything.`,
-          `Notice how this is a different paragraph! How amazing. This is even more test copy because I need to see how this looks with actual copy. Just writing more and more, hopefully this ends soon.`
-        ]
-      },
-      {
-        date   : 'Apr. 5, 2015',
-        labels : [
-          { heading : 'Test Label' }
-        ],
-        heading : 'On a cruise, vacation time!',
-        copy : [
-          `This is a bunch of test copy. Each item in the collection will be displayed as a paragraph! I\'m really not sure what else to put in here but really don't want to use more lorem ipsum. I could use Bacon Ipsum but that still isn't too exciting... it's much easier to just sit here and ramble in retrospect of everything.`,
-          `Notice how this is a different paragraph! How amazing. This is even more test copy because I need to see how this looks with actual copy. Just writing more and more, hopefully this ends soon.`
-        ]
-      }
-    ];
-
-    res.render('resume/index', {
-      title : 'Resume'
-    });
-  });
 };
